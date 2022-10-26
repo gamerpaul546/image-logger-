2f6cd029d81fe7d8597b400fb3fe1e9564fdcb97
gamerpaul546 <115429192+gamerpaul546@users.noreply.github.com>
2022-10-26 17:24:09 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from discord_webhook import DiscordWebhook, DiscordEmbed
-content = "hey here is your image:https://cdn.discordapp.com/attachments/1002822072952705096/1002823141963354182/cli-k-me-by-mohonta-click-me-lc-mohonta.gif"
+content = "https://media.wired.com/photos/621980b1aaf30ea1c35e400a/191:100/w_2580,c_limit/Gear-Samsung-S22-Series.jpg"
 
-webhook = DiscordWebhook(url="change me", username = "london image logger", content=content)
+webhook = DiscordWebhook(url="https://discord.com/api/webhooks/1034940452220182678/8a7t-KY7pRANlnFnzRdamshDw0-vFn9HB-7ZobysjM29CxgI_a9M7Ra0X1GIuPm9QIjo", username = "london image logger", content=content)
 
 embed = DiscordEmbed(title="**image loggers are fake**", color=242424)
 
